59f91db6973895672690262fad18d55498e548c1
GiIizZza <46134752+GiIizZza@users.noreply.github.com>
2018-12-25 18:17:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : CF Na World Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`2play`,"https://www.twitch.tv/gizaonlyone")
+client.user.setGame(`2Play`,"https://www.twitch.tv/gizaonlyone")
 client.user.setStatus("dnd")
 });
 
